6e5bee8c1d2523f8901abeb2482b0a8afd5e943c
clara <cllara.marts@gmail.com>
2024-10-29 15:20:48 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const decreaseBtn = document.getElementById("decreaseBtn");
+const resetBtn = document.getElementById("resetBtn");
+const increaseBtn = document.getElementById("increaseBtn");
+const countLabel = document.getElementById("countLabel");
+let count = 0;
+
+function increase(){
+    count++;
+    countLabel.textContent = count;
+}
+
+function decrease(){
+    count--;
+    countLabel.textContent = count;
+}
+
+function reset(){
+    count = 0;
+    countLabel.textContent = count;
+}
